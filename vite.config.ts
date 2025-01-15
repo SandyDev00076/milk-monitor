18e00f7db13f8803a664992a950044e6efb56fd2
Sanjeet Tiwari <sanjeettiwari00076@gmail.com>
2025-01-16 01:46:11 +0530

    change vite configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    assetsInlineLimit: 0, // Prevents inlining assets as base64 strings
+  },
+});
